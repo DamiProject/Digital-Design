14055b2d360e411b4c76f2dce100087e1d7e136b
DamiProject <Damilolaawotunde95@gmail.com>
2026-07-08 01:18:57 -0400

    Random and Address Counter

---
+++ VHDL/Dice/Counter.vhd
@@ -0,0 +1,44 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.Numeric_Std.all;
+
+
+------------------------------------------------------------------------------------
+-- This is a general-purpose counter module. It is reused twice in the main design:
+-- 1. As a 'random_counter' to generate the random dice roll.
+-- 2. As an 'address_counter' to step through the memory locations.
+-------------------------------------------------------------------------------------
+entity Counter is
+  port (  Reset  : in  std_logic;
+          Clock  : in  std_logic;
+          Enable : in  std_logic;
+          Q      : out std_logic_vector(2 downto 0));
+end entity Counter;
+
+
+architecture Rtl of Counter is
+
+  signal Count : unsigned(2 downto 0);
+
+begin
+
+  process(Clock, Reset)
+  begin
+    if Reset = '1' then
+      Count <= "001";
+    elsif rising_edge(Clock) then
+
+      if Enable = '1' then
+        if Count = "110" then
+          Count <= "001";
+        else
+          Count <= Count + 1;
+        end if;
+      end if;
+
+    end if;
+  end process;
+
+  Q <= std_logic_vector(Count);
+
+end architecture Rtl;
